a35e0cfaf2d574a1cf11f211bb74f66d4eb66820
Fran Bosquet <frbosquet@gmail.com>
2022-11-12 01:09:32 +0100

    add logo for iphone

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,8 @@ export default function Document() {
         <title>Boda de Cris y Fran</title>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#7F9867" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="7F9867" />
       </Head>
       <body>
         <Main />
